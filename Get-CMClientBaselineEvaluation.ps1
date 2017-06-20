133b3c8251a35159e566eced13709216035b3516
wasserja <wasserja@users.noreply.github.com>
2017-06-20 14:21:39 -0400

    1.4
    Added additional verbose output for troubleshooting.

--- Get-CMClientBaselineEvaluation.ps1
+++ Get-CMClientBaselineEvaluation.ps1
@@ -1,9 +1,10 @@
 ﻿<#
-.Synopsis
+.SYNOPSIS
    Get the status of Configuration Manager client configuration baselines.
 
 .DESCRIPTION
    Get the status of Configuration Manager client configuration baselines.
+
 .NOTES
    Created by: Jason Wasser @wasserja
    Modified: 5/17/2017 04:05:12 PM 
@@ -65,6 +66,7 @@ function Get-CMClientBaselineEvaluation
     process {
         foreach ($Computer in $ComputerName) {
             # Get a list of baseline objects assigned to the remote computer
+            Write-Verbose -Message "Attempting to get Configuration Baselines for $Computer"
             $Baselines = Get-WmiObject -ComputerName $Computer -Namespace root\ccm\dcm -Class SMS_DesiredConfiguration
 
             # For each (%) baseline object, call SMS_DesiredConfiguration.TriggerEvaluation, passing in the Name and Version as params
